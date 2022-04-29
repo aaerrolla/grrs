81a11b8d21f4bd6aaf1d608991be0b4619f11470
Anjan <aaerrolla@gmail.com>
2022-04-29 20:58:23 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let pattern = std::env::args().nth(1)
+        .expect("no pattern given");
+    let path = std::env::args().nth(2)
+        .expect("no path given");
+    let content = std::fs::read_to_string(&path)
+        .expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&pattern) {
+            println!("{}", line );
+        }
+    }
+}
