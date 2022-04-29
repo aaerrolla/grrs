54196b272c114f1d62cbdfbb81762fd0067302b1
Anjan <aaerrolla@gmail.com>
2022-04-29 22:30:31 +0530

    introduced struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+// feature-1
+// introduced struct , but still not bestway  to handle command line 
+// arguments
+
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
     let pattern = std::env::args().nth(1)
         .expect("no pattern given");
     let path = std::env::args().nth(2)
         .expect("no path given");
-    let content = std::fs::read_to_string(&path)
+
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    let content = std::fs::read_to_string(&args.path)
         .expect("could not read file");
 
     for line in content.lines() {
-        if line.contains(&pattern) {
+        if line.contains(&args.pattern) {
             println!("{}", line );
         }
     }
