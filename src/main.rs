331f4753f0ca3c058cefab35bd00b41e801e35f7
Anjan <aaerrolla@gmail.com>
2022-04-30 12:33:45 +0530

    [feature-3]-variation to use BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-// feature-2
+// feature-3 - using BufReader
 // using clap  https://docs.rs/clap/   to handle command line arguments
 // clap supports subcommands, shell completions and good help messages
 
+// Exercise for the reader: This is not the best implementation: 
+// It will read the whole file into memory – however large the file may be. 
+// Find a way to optimize it! 
+// (One idea might be to use a BufReader (https://doc.rust-lang.org/1.39.0/std/io/struct.BufReader.html) instead of read_to_string().)
+
 use clap::Parser;
+use std::io::prelude::BufRead;
+use std::io::BufReader;
+use std::fs::File;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -15,20 +23,23 @@ struct Cli {
 }
 
 fn main() {
-    let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path);
-    match content {
-        Ok(content) => {
-            for line in content.lines() {
+    let args = Cli::parse();   
+   
+    // use BufReader
+    let file = File::open(&args.path);
+    match file {
+        Ok(file) => {
+            let mut reader = BufReader::new(file);
+            let mut line =  String::new();
+            while reader.read_line(&mut line).expect("unable to read") != 0 {
                 if line.contains(&args.pattern) {
                     println!("{}", line );
                 }
+                line.clear();
             }
-        
         }
         Err(error) => {
-            println!("Error {} ", error);
+            println!("Error {} ", error);    
         }
     }
-           
 }
