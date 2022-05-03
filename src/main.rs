853085b872cabfd94a60e5dc1e8acde94943b8ca
Anjan <aaerrolla@gmail.com>
2022-05-03 10:49:13 +0530

    [feature-6] - introducing custom error type  to provide context

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-// feature-5 - error handling 
-// in this demo we will 
-// 1 instead of  panic!  or  unwrap()  to handle error condition 
-// 2  we can return Err from function
-// 3 using shortcut ?  to return Err
-
+// feature-6 - providing context in the logs
+// The errors you get when using ? in your main function are okay, but they are not great. 
+// For example: When you run std::fs::read_to_string("test.txt")? but the file test.txt doesn’t exist, you get this output:
+// Error: Os { code: 2, kind: NotFound, message: "No such file or directory" }
+// For example, we can create our own error type, and then use that to build a custom error message:
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -16,53 +15,22 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
-    
-    // handle_cmd_wihout_return();    
-    // handle_cmd_with_return().expect("Error reading ");  
+
+fn main() {    
     handle_cmd_with_return_shortcut().expect("Error reading ");  
 }
 
-// fn handle_cmd_wihout_return() {
-//     let args = Cli::parse();   
-
-//     let result = std::fs::read_to_string(&args.path);
-//     let content = match result {
-//         Ok(content) => {
-//             content
-//         },
-//         Err(error) => {
-//             return Err(error.into());
-//         }
-//     }; 
-//     println!("file content: {}", content);
-//     Ok(()) 
-// }
-
-///  we declare this function return type has Result
-///  and return Error  and  Ok 
-fn handle_cmd_with_return() -> Result<(), Box<dyn std::error::Error> > {
-    let args = Cli::parse();   
-
-    let result = std::fs::read_to_string(&args.path);
-    let content = match result {
-        Ok(content) => {
-            content
-        },
-        Err(error) => {
-            return Err(error.into());
-        }
-    }; 
-    println!("file content: {}", content);
-    Ok(()) 
-}
 
 ///  we declare this function return type has Result///  
 ///  using shortcut form ?  to return from Err
-fn handle_cmd_with_return_shortcut() -> Result<(), Box<dyn std::error::Error> > {
+#[derive(Debug)]
+struct CustomError(String);
+
+fn handle_cmd_with_return_shortcut() -> Result<(), CustomError > {
     let args = Cli::parse();   
 
-    let content = std::fs::read_to_string(&args.path)?;
+    let content = std::fs::read_to_string(&args.path)
+        .map_err(|err| CustomError(format!("Error reading `{:?}`: {}", &args.path, err)))?;
     println!("file content: {}", content);
     Ok(())
 }
